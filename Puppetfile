dfd796c9ab3f6172f9dcbaf804b9d8d3f3a209c2
Jordan Pitlor <jordan.pitlor@avasure.com>
2024-02-05 09:37:48 -0500

    wip

--- Puppetfile
+++ Puppetfile
@@ -1,1 +1,2 @@
 mod 'puppetlabs-host_core', '1.2.0'
+mod 'puppetlabs-vcsrepo', '6.1.0'
--- site-modules/profile/manifests/internal_dns.pp
+++ site-modules/profile/manifests/internal_dns.pp
@@ -1,14 +1,22 @@
 # Sets up hosts file to resolve internal services
 class profile::internal_dns {
   host { 'NAS':
-    name    => 'nas.pitlor.local',
-    ip      => '10.1.0.2',
-    comment => 'port 5001',
+    name => 'nas.pitlor.local',
+    ip   => '10.1.0.7',
   }
 
   host { 'Nginx Proxy Manager':
-    name    => 'proxy.pitlor.local',
-    ip      => '10.1.0.7',
-    comment => 'port 81',
+    name => 'proxy.pitlor.local',
+    ip   => '10.1.0.7',
+  }
+
+  host { 'Proxmox':
+    name => 'proxmox.pitlor.local',
+    ip   => '10.1.0.7',
+  }
+
+  host { 'Portainer':
+    name => 'portainer.pitlor.local',
+    ip   => '10.1.0.7',
   }
 }
