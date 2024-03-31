e85c724a3bdc85aeeca96c90888f78dea8c39cbe
Jordan Pitlor <jordan@pitlor.dev>
2024-03-31 19:32:47 -0400

    Added some stuff for docker

---
+++ site-modules/profile/manifests/docker_host.pp
@@ -0,0 +1,25 @@
+# Use homelab-containers repository to deploy docker containers
+class profile::docker_host {
+  include 'docker'
+
+  class {'docker::compose':
+    ensure  => present,
+    version => '1.9.0',
+  }
+
+  package { 'git':
+    ensure => installed,
+  }
+
+  vcsrepo { '/var/data/dev.pitlor.homelab-containers':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/jpitlor/homelab-containers',
+    revision => 'TODO',
+  }
+
+  docker_compose { 'homelab':
+    compose_files => ['/var/data/dev.pitlor.homelab-containers/docker-compose.yml'],
+    ensure        => present,
+  }
+}
